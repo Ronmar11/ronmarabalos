6624ae91226fe040035ae9eacaa9ac50771d5019
Ronmar11 <109712770+Ronmar11@users.noreply.github.com>
2025-11-23 22:21:25 +0800

    fixing grid layout

--- script.js
+++ script.js
@@ -1,18 +1,12 @@
 const toggle = document.getElementById("darkToggle");
 const facebook = document.getElementById('facebook-link');
 const github = document.getElementById('github-link');
-const porfolio = document.getElementById('porfolio-link');
 const meal = document.getElementById('mealmaster-link');
 const bms = document.getElementById('bms-link');
 const pra = document.getElementById('pra-link');
 const linkedid = document.getElementById('linkedid-link');
 const github2 = document.getElementById('github-link2');
 const instagram = document.getElementById('instagram-link');
-const email = document.getElementById('email-link');
-const bars = document.querySelector('.bars');
-const menu = document.querySelector('.menu');
-const bmsyt = document.getElementById('bms-yt-link');
-const prayt = document.getElementById('pra-yt-link');
 
 
 toggle.addEventListener("change", () => {
@@ -33,14 +27,6 @@ document.addEventListener('DOMContentLoaded', function() {
       github.addEventListener('click', function() {
         const url = this.getAttribute('data-url');
         if(url){
-          window.open(url);
-        }
-      });
-    }
-    if(porfolio){
-      porfolio.addEventListener('click', function(){
-        const url = this.getAttribute('data-url');
-        if(porfolio){
           window.open(url);
         }
       });
@@ -71,22 +57,6 @@ document.addEventListener('DOMContentLoaded', function() {
         }
       });
     }
-        if(bmsyt){
-      bmsyt.addEventListener('click', function(){
-        const url = this.getAttribute('data-url');
-        if(bmsyt){
-          window.open(url);
-        }
-      });
-    }
-    if(prayt){
-      prayt.addEventListener('click', function(){
-        const url = this.getAttribute('data-url');
-        if(prayt){
-          window.open(url);
-        }
-      });
-    }
     if(linkedid){
       linkedid.addEventListener('click', function(){
         const url = this.getAttribute('data-url');
@@ -109,24 +79,6 @@ document.addEventListener('DOMContentLoaded', function() {
         if(instagram){
           window.open(url);
         }
-      });
-    }
-    if(email){
-      email.addEventListener('click', function(){
-        const url = this.getAttribute('data-url');
-        if(email){
-          window.open(`mailto:${url}`)
-        }
-      });
-    }
-    if (bars && menu) {
-      bars.addEventListener('click', function() {
-        menu.classList.toggle('active');
-      });
-      menu.querySelectorAll('a').forEach(function(link) {
-        link.addEventListener('click', function() {
-          menu.classList.remove('active');
-        });
       });
     }
 });
